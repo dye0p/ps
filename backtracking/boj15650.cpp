553ae6eed246758a93b31b5bdebfaaf96d094e98
DongYeop <s01054830455@gmail.com>
2024-03-02 02:00:40 +0900

    백트래킹 - N과 M(2) next_permutation 사용

--- backtracking/boj15650.cpp
+++ backtracking/boj15650.cpp
@@ -6,36 +6,25 @@
 using namespace std;
 
 int n, m;
-int arr[10]; //길이가 M인 수열을 담을 배열
-bool isused[10]; //기본 false로 초기화
-
-void func(int k) { //k의 범위는 m까지
-    //base Condition
-    if (k == m) {
-        for (int i = 0; i < m; i++) {
-            cout << arr[i] << ' ';
-        }
-        cout << '\n';
-        return;
-    }
-    int start = 1;
-    if(k!=0) {
-        start = arr[k-1] + 1;
-    }
-    for (int i = start; i <= n; i++) {
-        if (isused[i] != 1) {
-            arr[k] = i; //k번째 위치에 i를 추가
-            isused[i] = 1; //해당 숫자 사용완료(중복 방지)
-            func(k + 1);
-            isused[i] = 0;
-        }
-    }
-}
+vector<int> a;
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
     cin >> n >> m;
-    func(0);
+
+    for (int i = 0; i < n; i++) {
+        a.push_back(i < m ? 0 : 1);
+    }
+
+    do {
+        for (int i = 0; i < n; i++) {
+            if (a[i] == 0) {
+                cout << i + 1 << ' ';
+            }
+        }
+        cout << '\n';
+    } while (next_permutation(a.begin(), a.end()));
+
 }
