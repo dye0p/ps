56fbe26026f0f5fe8cb2bc5057482cdf33735ba4
DongYeop <s01054830455@gmail.com>
2024-05-02 14:45:46 +0900

    기초 - A+B

---
+++ basics/boj1000.cpp
@@ -0,0 +1,18 @@
+//
+// PS on 24. 5. 2, 오후 2:44.
+//
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int a,b;
+
+    cin >> a >>b;
+
+    cout << a + b;
+
+}
