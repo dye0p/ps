df67178172d184fc213eb9ab6079efab7528ca70
DongYeop <s01054830455@gmail.com>
2024-05-05 01:13:13 +0900

    기초 - A / B

---
+++ basics/boj1008.cpp
@@ -0,0 +1,19 @@
+//
+// PS on 24. 5. 5, 오전 1:11.
+//
+#include <bits/stdc++.h>
+
+using namespace std;
+
+double a, b;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> a >> b;
+
+    cout.precision(12);
+    cout << fixed;
+    cout << a / b;
+}
