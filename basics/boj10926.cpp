9a6bfe0d69bbcf5201da8a1c42580c6a4cf5b94b
DongYeop <s01054830455@gmail.com>
2024-05-06 02:45:42 +0900

    기초 - ??!

---
+++ basics/boj10926.cpp
@@ -0,0 +1,16 @@
+//
+// PS on 24. 5. 6, 오전 2:44.
+//
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string s;
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> s;
+
+    cout << s << "??!";
+}
