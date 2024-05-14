032acdbd53c0d8fdb025eb5a2f3bbf2e29108ed5
DongYeop <s01054830455@gmail.com>
2024-05-15 01:20:42 +0900

    기본 - 문자와 문자열

---
+++ basics/boj27866.cpp
@@ -0,0 +1,16 @@
+//
+// 24. 5. 15, 오전 1:14.
+//
+#include <bits/stdc++.h>
+
+using namespace std;
+string s;
+int n;
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> s >> n;
+    cout << s[n - 1];
+
+}
