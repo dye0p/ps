33ef405eb381d2fd1fe6e0f3b1d3e4db7d51aa07
DongYeop <s01054830455@gmail.com>
2024-05-16 21:15:41 +0900

    기본 - 아스키 코드

---
+++ basics/boj11654.cpp
@@ -0,0 +1,18 @@
+//
+// 24. 5. 16, 오후 9:11.
+//
+#include <bits/stdc++.h>
+
+using namespace std;
+
+char a;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> a;
+    cout << (int) a;
+    //char 타입은 문자를 정수형태로 저장한다. 이 때 사용되는 기준이 ascii 코드표이다
+    //따라서 저장된 ascii 코드번호를 정수 그대로 출력받기 위해서는 변수타입을 int로 변경해주면 된다.
+}
