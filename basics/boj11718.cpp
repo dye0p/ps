69fff2709dc65dd88fbe5c41847cf93d55fccee7
DongYeop <s01054830455@gmail.com>
2024-05-22 01:04:50 +0900

    문자열 - 그대로 출력하기

---
+++ basics/boj11718.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    for (int i = 0; i < 100; i++) {
+        string s;
+        getline(cin, s);
+        cout << s << '\n';
+    }
+}
